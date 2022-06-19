49e9910a3ae807870e1df0358e08ffc2305e8fae
hugodnpm <hugosc@gmail.com>
2022-06-19 15:16:06 -0300

    modificado hello.py

--- hello.py
+++ hello.py
@@ -8,6 +8,10 @@ Tenha a variável LANG devidamente configurada ex:
 
     export LANG=pt_BR
 
+Ou informe através do CLI argument "--lang"
+
+Ou o usuário terá que digitar.
+
 Execucação:
 
     python3 hello.py
@@ -19,13 +23,36 @@ __author__ = "Hugo Carvalho"
 __license__ = "unlicense"
 
 import os
+import sys
+
+arguments = {
+    "lang": None,
+    "count": 1
+}
+for arg in sys.argv[1:]:
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print(f"Invalid Option `{key}`")
+        sys.exit()
+    arguments[key] = value
+
+
 
 
 # Script hello World 
 # - comentário de linha
 #snake case = snake_case
 #Pascal Case = PascalCase
-current_language = os.getenv("LANG","en_US")[:5] 
+current_language = arguments["lang"]
+if current_language is None:
+    if "LANG" in os.environ:
+         current_language = os.getenv("LANG")
+    else:
+        current_language = input("Choose your language:")
+
+current_language = current_language[:5]
 msg = {
     "en_US": "Hello, World!",
     "pt_BR": "Olá, Mundo!",
@@ -35,4 +62,4 @@ msg = {
 }
 
 
-print(msg[current_language])
+print(msg[current_language] * int(arguments["count"]))
